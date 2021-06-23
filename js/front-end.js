18605d363d01bf2b6a2709eff69e5a1b67152230
Rishav Kumar <rishavraj26062001@gamil.com>
2021-06-23 13:37:16 +0530

    reduced error

--- js/front-end.js
+++ js/front-end.js
@@ -43,40 +43,6 @@ nextarw.addEventListener("click", () => {
 }
 )
 
-var number = /^[6-9]\d{9}/s
-var fname = /^[A-Z][a-z]{2,10}/s
-var lname = /^[A-Z][a-z]{2,10}/s
-var password = /^[a-zA-z0-9\.$%@]{8,20}/s
-var phone = /^[6-9]\d{9}/s
-var email = /^([a-zA-z0-9\.-]+)@([a-zA-z0-9-]+)(\.)[a-z]{2,20}/s
-console.log(document.forms[frmname]["fname"].value)
-function isvalid(frmname) {
-
-    if (!fname.test(document.forms[frmname]["fname"].value)) {
-        
-        alert("Name must contain only letters and first letter must be capital.")
-        return false
-    }
-    if (!lname.test(document.forms[frmname]["lname"].value)) {
-        alert("Name must contain only letters and first letter must be capital.")
-        return false
-    }
-    if (!email.test(document.forms[frmname]["email"].value)) {
-        alert("invalid mail")
-        return false
-    }
-
-    if (!phone.test(document.forms[frmname]["contact"].value)) {
-        alert("No. must have 10 digits only")
-        return false
-    }
-
-    if (!password.test(document.forms[frmname]["password"].value)) {
-        alert("password must contain atleast 8 characters.")
-        return false
-    }
-}
-
 
 function goto() {
     var city = document.querySelector("#browse>section>input").value.toLowerCase()
